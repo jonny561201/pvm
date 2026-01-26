f178f1fc53ecb7e0594bd75314ad06c5aa532033
Jon Graf <jgraf@vermeer.com>
2026-01-25 19:56:29 -0600

    created function to make sure pvm directory exists...not sure I needed it

--- svc/services/folder_service.py
+++ svc/services/folder_service.py
@@ -1,4 +1,17 @@
 from pathlib import Path
+import os
+
+from svc.constants.file_constants import File
+
+
+def create_pvm_directory():
+    pvm_dir = Path.home() / ".pvm"
+    pvm_dir.mkdir(parents=True, exist_ok=True)
+    try:
+        os.chmod(pvm_dir, File.read_write_exec)
+    except PermissionError:
+        pass
+    return pvm_dir
 
 
 def delete_tar_file(pvm_dir: Path, filename: str):
