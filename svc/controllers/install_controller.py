cedb246333d3b641321e48993b4b6b1f0066ca7f
Jon Graf <jgraf@vermeer.com>
2026-01-25 20:41:28 -0600

    converted the install orchestration functions to save to versions folder

--- svc/controllers/install_controller.py
+++ svc/controllers/install_controller.py
@@ -6,14 +6,14 @@ from svc.services.install_service import download_python, extract_zip
 def install_orchestration(version: str):
     file_name = f"{version}.tgz"
     print('...downloading python version...')
-    download_python(File.PVM_DIR, version, file_name)
+    download_python(File.VERSION_DIR, version, file_name)
     print('...extracting python version...')
-    extract_zip(File.PVM_DIR, file_name)
-    delete_tar_file(File.PVM_DIR, file_name)
+    extract_zip(File.VERSION_DIR, file_name)
+    delete_tar_file(File.VERSION_DIR, file_name)
 
 
 def get_python_versions():
-    directories = get_python_version_folders(File.pvm_dir)
+    directories = get_python_version_folders(File.PVM_DIR)
     versions = sorted([directory.name for directory in directories])
     for version in versions:
         print(version)
