bf82a7e8f7152c5ef7eafa167dcb83cad058c18d
Jon Graf <jgraf@vermeer.com>
2026-01-30 15:56:14 -0600

    took a stab at building out a use function for the script

--- svc/services/command_service.py
+++ svc/services/command_service.py
@@ -1,5 +1,9 @@
+import os
+import re
+
 from svc.constants.file_constants import File, Architecture, OS
-from svc.utilities.folder_utils import get_python_version_folders, set_global_python, create_version_directory
+from svc.utilities.folder_utils import get_python_version_folders, set_global_python, create_version_directory, \
+    find_python_version
 from svc.utilities.install_utils import download_python_release, extract_zip, delete_tar_file
 from svc.utilities.prebuilt_release_utils import get_python_release_tag, get_python_releases, find_python_release
 
@@ -10,6 +14,7 @@ def install_latest_release(version: str):
     release = find_python_release(releases, version, OS.APPLE, Architecture.INTEL)
     file_name = f"{version}.tgz"
     folder_name = create_version_directory(release)
+
     directory = File.VERSION_DIR / folder_name
     download_python_release(release, directory, file_name)
     extract_zip(directory, file_name)
@@ -29,4 +34,18 @@ def set_default_version(version: str):
 
 
 def use_python_version(version: str):
-    print(f"pvm: using python version {version}")
+    folder = find_python_version(version)
+    executable = folder / "bin" / "python"
+    paths = os.environ.get('PATH')
+    update_paths = _remove_existing_versions_from_path(paths)
+    print(f"export PATH={executable.absolute()}:{update_paths}")
+    print(f'export PVM_VERSION="{folder.name}"')
+    print(f"pvm: using python version {folder.name}")
+
+
+def _remove_existing_versions_from_path(paths: str):
+    re.sub(r"(?:^|:)~/.pvm/versions/python-[^:/]+/bin", "", paths)
+    paths.strip(":")
+    re.sub(r":{2,}", ":", paths)
+
+    return paths
